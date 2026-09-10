535fa21f99118fdd6d7baa752b905a81979d3919
Raul Hernandez <optimusgamer55hdz@gmail.com>
2026-09-10 08:35:58 -0600

    funcion de recomendacion agregada

--- DAM_Semana1_p1/bin/actividadIndependiente.dart
+++ DAM_Semana1_p1/bin/actividadIndependiente.dart
@@ -16,3 +16,15 @@ String determinarEstado(double promedio, double asistencia) {
     return 'Reprobado';
   }
 }
+
+String generarRecomendacion(double promedio, double asistencia) {
+  if (asistencia < 75) {
+    return 'Debe mejorar su asistencia para tener derecho a evaluacion.';
+  } else if (promedio >= 7) {
+    return 'Excelente desempeño siga asi';
+  } else if (promedio >= 5) {
+    return 'Refuerce sus estudios para aprobar su recuperacion';
+  } else {
+    return 'Necesita apoyo academico intensivo.';
+  }
+}
