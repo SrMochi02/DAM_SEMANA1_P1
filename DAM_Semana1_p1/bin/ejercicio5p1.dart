9e448ce212336a4663cb7ef518ceff5fd277a1cc
Raul Hernandez <optimusgamer55hdz@gmail.com>
2026-09-10 07:56:12 -0600

    2do cambio condicion invertida corregida

--- DAM_Semana1_p1/bin/ejercicio5p1.dart
+++ DAM_Semana1_p1/bin/ejercicio5p1.dart
@@ -2,13 +2,11 @@ void main() {
   //cambio 1 tipo correcto para el divisor 
   //el divisor nunca puede ser 0 asi que se le asigna valor al divisor.
   double divisor = 2.0;
-  
-  
   int numero = 10;
-  
   double? resultado;
 
-  if (numero < 5) { // condición invertida
+  //cambio 2
+  if (numero > 5) {
     resultado = numero / divisor; // división por cero y división entera no deseada
   }
 
