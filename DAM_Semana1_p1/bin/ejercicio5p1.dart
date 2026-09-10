a1911ba69f193d73e201d29c3d1e64906314e414
Raul Hernandez <optimusgamer55hdz@gmail.com>
2026-09-10 07:58:53 -0600

    cambio 3 division corregida antes era division entera no deseada

--- DAM_Semana1_p1/bin/ejercicio5p1.dart
+++ DAM_Semana1_p1/bin/ejercicio5p1.dart
@@ -1,13 +1,13 @@
 void main() {
-  //cambio 1 tipo correcto para el divisor 
+  //cambio 1 tipo correcto para el divisor
   //el divisor nunca puede ser 0 asi que se le asigna valor al divisor.
   double divisor = 2.0;
   int numero = 10;
   double? resultado;
-
   //cambio 2
   if (numero > 5) {
-    resultado = numero / divisor; // división por cero y división entera no deseada
+    //cambio 3
+    double resultado = numero / divisor;
   }
 
   String x = "Hola"; // nombre poco descriptivo
