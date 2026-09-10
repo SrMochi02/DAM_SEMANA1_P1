7bd87a5afe7582e97b10b7010c198d6af28dec61
Raul Hernandez <optimusgamer55hdz@gmail.com>
2026-09-10 08:30:21 -0600

    funcion para comprobar estado academico

--- DAM_Semana1_p1/bin/actividadIndependiente.dart
+++ DAM_Semana1_p1/bin/actividadIndependiente.dart
@@ -1,7 +1,18 @@
+import 'dart:developer';
 import 'dart:io';
 
 double calcularPromedio(double n1, double n2, double n3) {
   return (n1 + n2 + n3);
 }
 
-
+String determinarEstado(double promedio, double asistencia) {
+  if (asistencia < 75) {
+    return 'Sin derecho';
+  } else if (promedio >= 7) {
+    return 'Aprobado';
+  } else if (promedio >= 5) {
+    return 'En recuperacion';
+  } else {
+    return 'Reprobado';
+  }
+}
